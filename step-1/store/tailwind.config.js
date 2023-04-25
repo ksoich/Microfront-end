39ae642afaf895dda019f9c44d9aab5402b3b2b6
Soich <KSoich@wbmi.com>
2023-04-24 20:04:02 -0500

    update tailwind config

--- step-1/store/tailwind.config.js
+++ step-1/store/tailwind.config.js
@@ -1,15 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', '../products/src/**/**.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-  safelist: [
-    {
-      pattern:
-        /^(bg-|border-|text-|italic|mb|pr-|pl-|py-|font-|align-|flex|justify-|transition|duration-|ease-in-out|transform|scale-|p-|relative|overflow-|col-|mx-|my-|grid|grid-|gap-)/,
-      variants: ['hover', 'active'],
-    },
-  ],
+  plugins: []
 };
--- step-final/store/src/pages/ProductPage.tsx
+++ step-final/store/src/pages/ProductPage.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import ProductList from 'MobilePhoneProducts/ProductList';
+
+const ProductList = React.lazy(() => import('MobilePhoneProducts/ProductList'));
+import { ErrorBoundary } from 'react-error-boundary';
+
 export const ProductPage = () => {
   return (
     <main className="py-5 max-w-screen-lg m-auto px-4">
       <h1 className="m-auto font-extrabold text-5xl text-center">Our Products</h1>
-      <React.Suspense>
-        <ProductList />
-      </React.Suspense>
+      <ErrorBoundary fallback={<div>Store is temporarily down</div>}>
+        <React.Suspense>
+          <ProductList />
+        </React.Suspense>
+      </ErrorBoundary>
     </main>
   );
 };
